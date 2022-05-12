356722b20480a3c3942c68a28da68f1163cf6e95
atsumi <atsumi.amaya@tecsup.edu.pe>
2022-05-12 18:11:11 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio9/Ejercicio9/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Ejercicio9
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new Explicit();
+            //MainPage = new ExplicitControl();
+            //MainPage = new Implicit();
+            //MainPage = new ImplicitControl();
+            //MainPage = new Global();
+            //MainPage = new Inheritance();
+            MainPage = new Dynamic();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Ejercicio9/Ejercicio9/Dynamic.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio9
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Dynamic : ContentPage
+	{
+        bool originalStyle = true;
+		public Dynamic ()
+		{
+			InitializeComponent ();
+            Resources["SearchBarStyle"] = Resources["blueSearchBarStyle"];
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (originalStyle)
+            {
+                Resources["SearchBarStyle"] = Resources["greenSearchBarStyle"];
+                originalStyle = false;
+            }
+            else
+            {
+                Resources["SearchBarStyle"] = Resources["blueSearchBarStyle"];
+                originalStyle = true;
+            }
+        }
+	}
+}
